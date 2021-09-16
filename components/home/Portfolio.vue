1c703725c554da0d6e07664a112cc305ce671296
William Yallop <hello@williamyallop.com>
2021-09-16 21:30:53 +0100

    Updated project data in portfolio component

--- components/home/Portfolio.vue
+++ components/home/Portfolio.vue
@@ -1,99 +1,18 @@
 <template>
     <div class="gridCon">
 
-        <div class="row">
-            <div class="projectCon"
-            :class="{ 'nonFocus': hoverState && activeClass != 0, 'highlight': activeClass === 0  }"
-            @mouseover="projectOver(true, 0)" @mouseleave="projectOver(false, 0)">
-
+        <div class="row" :key="row.id" v-for="row in projects">
+            <div class="projectCon" :key="project.id" v-for="project in row.projects"
+            :class="{ 'nonFocus': hoverState && activeClass != project.id, 'highlight': activeClass === project.id  }"
+            @mouseover="projectOver(true, project.id)" @mouseleave="projectOver(false, project.id)">
                 <div class="textareaCon">
-                    <h3 class="typography-portfolio">Playlist Cover<br>Maker Tool</h3>
+                    <h3 class="typography-portfolio">{{ project.title[0] }}<br>{{ project.title[1] }}</h3>
                     <button class="btnMain">find out more</button>
                 </div>  
                 <div class="imageCon">
-                    <img class="deskImg" src="@/images/projects/bassDesktop.png" alt="">
-                    <img class="mobImg" src="@/images/projects/bassMobile.png" alt="">
+                    <img class="deskImg" :src="project.images.desktop" :alt="project.images.alt">
+                    <img class="mobImg" :src="project.images.mobile" :alt="project.images.alt">
                 </div>
-
-            </div>
-
-            <div class="projectCon" 
-            :class="{ 'nonFocus': hoverState && activeClass != 1, 'highlight': activeClass === 1  }"
-            @mouseover="projectOver(true, 1)" @mouseleave="projectOver(false, 1)">
-
-                <div class="textareaCon">
-                    <h3 class="typography-portfolio">Playlist<br>Finder</h3>
-                    <button class="btnMain">find out more</button>
-                </div>  
-                <div class="imageCon">
-                    <img class="deskImg" src="@/images/projects/bassDesktop.png" alt="">
-                    <img class="mobImg" src="@/images/projects/bassMobile.png" alt="">
-                </div>
-
-            </div>
-        </div>
-
-        <div class="row alt">
-            <div class="projectCon" 
-            :class="{ 'nonFocus': hoverState && activeClass != 2, 'highlight': activeClass === 2  }"
-            @mouseover="projectOver(true, 2)" @mouseleave="projectOver(false, 2)">
-
-                <div class="textareaCon">
-                    <h3 class="typography-portfolio">Melody<br>Melon</h3>
-                    <button class="btnMain">find out more</button>
-                </div>  
-                <div class="imageCon">
-                    <img class="deskImg" src="@/images/projects/bassDesktop.png" alt="">
-                    <img class="mobImg" src="@/images/projects/bassMobile.png" alt="">
-                </div>
-
-            </div>
-
-            <div class="projectCon" 
-            :class="{ 'nonFocus': hoverState && activeClass != 3, 'highlight': activeClass === 3  }"
-            @mouseover="projectOver(true, 3)" @mouseleave="projectOver(false, 3)">
-
-                <div class="textareaCon">
-                    <h3 class="typography-portfolio">Playlist<br>Tools</h3>
-                    <button class="btnMain">find out more</button>
-                </div>  
-                <div class="imageCon">
-                    <img class="deskImg" src="@/images/projects/bassDesktop.png" alt="">
-                    <img class="mobImg" src="@/images/projects/bassMobile.png" alt="">
-                </div>
-
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="projectCon" 
-            :class="{ 'nonFocus': hoverState && activeClass != 4, 'highlight': activeClass === 4  }"
-            @mouseover="projectOver(true, 4)" @mouseleave="projectOver(false, 4)">
-
-                <div class="textareaCon">
-                    <h3 class="typography-portfolio">EXODIA<br>Fitness</h3>
-                    <button class="btnMain">find out more</button>
-                </div>  
-                <div class="imageCon">
-                    <img class="deskImg" src="@/images/projects/bassDesktop.png" alt="">
-                    <img class="mobImg" src="@/images/projects/bassMobile.png" alt="">
-                </div>
-
-            </div>
-
-            <div class="projectCon" 
-            :class="{ 'nonFocus': hoverState && activeClass != 5, 'highlight': activeClass === 5 }"
-            @mouseover="projectOver(true, 5)" @mouseleave="projectOver(false, 5)">
-
-                <div class="textareaCon">
-                    <h3 class="typography-portfolio">Bassline<br>Promotions</h3>
-                    <button class="btnMain">find out more</button>
-                </div>  
-                <div class="imageCon">
-                    <img class="deskImg" src="@/images/projects/bassDesktop.png" alt="">
-                    <img class="mobImg" src="@/images/projects/bassMobile.png" alt="">
-                </div>
-
             </div>
         </div>
 
@@ -105,12 +24,77 @@ export default {
     data() {
         return {
             hoverState: false,
+            activeClass: false,
 
             projects: [
-
-
+                { id: 0, projects: [   
+                    {
+                        id: 0,
+                        title: ['Playlist Cover', 'Maker Tool'],
+                        href: '',
+                        images: {
+                            alt: '',
+                            desktop: require('@/images/projects/bassDesktop.png'),
+                            mobile: require('@/images/projects/bassMobile.png')
+                        }
+                    },
+                    {
+                        id: 1,
+                        title: ['Playlist', 'Finder'],
+                        href: '',
+                        images: {
+                            alt: '',
+                            desktop: require('@/images/projects/bassDesktop.png'),
+                            mobile: require('@/images/projects/bassMobile.png')
+                        }
+                    },
+                ]},
+                
+                { id: 1, projects: [
+                    {
+                        id: 2,
+                        title: ['Melody', 'Melon'],
+                        href: '',
+                        images: {
+                            alt: '',
+                            desktop: require('@/images/projects/bassDesktop.png'),
+                            mobile: require('@/images/projects/bassMobile.png')
+                        }
+                    },
+                    {
+                        id: 3,
+                        title: ['Playlist', 'Tools'],
+                        href: '',
+                        images: {
+                            alt: '',
+                            desktop: require('@/images/projects/bassDesktop.png'),
+                            mobile: require('@/images/projects/bassMobile.png')
+                        }
+                    },
+                ]},
+                { id: 2, projects: [
+                    {
+                        id: 4,
+                        title: ['EXODIA', 'Fitness'],
+                        href: '',
+                        images: {
+                            alt: '',
+                            desktop: require('@/images/projects/bassDesktop.png'),
+                            mobile: require('@/images/projects/bassMobile.png')
+                        }
+                    },
+                    {
+                        id: 5,
+                        title: ['Bassline', 'Promotions'],
+                        href: '',
+                        images: {
+                            alt: '',
+                            desktop: require('@/images/projects/bassDesktop.png'),
+                            mobile: require('@/images/projects/bassMobile.png')
+                        }
+                    }
+                ]}
             ]
-
 
         }
     },
@@ -133,13 +117,10 @@ export default {
         grid-template-columns: 6fr 4fr;
         grid-template-rows: auto auto;
         gap: 20px;
-        &.alt {
-            grid-template-columns: 4fr 6fr;
-        }
         &:last-child {
             margin-bottom: 0;
         }
-        @media only screen and (max-width: 1200px) {
+        @media only screen and (max-width: 1400px) {
             grid-template-columns: 1fr !important;
             margin-bottom: 20px;
         }
@@ -172,6 +153,7 @@ export default {
             }
         }
         &:nth-child(even) {
+            grid-template-columns: 4fr 6fr;
             // Right
             .projectCon {
                 &:nth-child(1) {
@@ -232,15 +214,24 @@ export default {
             &.highlight {
                 transform: scale(103%);
             }
+            &:hover {
+                .imageCon {
+                    .deskImg {
+                        transform: scale(1.1) rotate(2deg);
+                    } 
+                    .mobImg {
+                        transform: translateY(calc(25% - 10px)) scale(1.1) rotate(2deg);
+                    }
+                }
+            }
             // Inner
-
             .textareaCon {
                 position: absolute;
                 top: 0;
                 left: 0;
                 bottom: 0;
                 min-width: 50%;
-                padding: 50px;
+                padding: 30px;
                 border-radius: 20px 0px 0px 20px;
                 background: rgb(0,0,0);
                 background: linear-gradient(90deg, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0) 100%);
@@ -271,10 +262,14 @@ export default {
                 padding: 40px;
                 z-index: 0;
                 overflow: hidden;
+                .deskImg,
+                .mobImg {
+                    transition: 0.3s ease-in-out;
+                }
                 .deskImg {
                     max-width: 80%;
                     display: none;
-                    @media only screen and (max-width: 1200px) {
+                    @media only screen and (max-width: 1400px) {
                         display: block !important;
                     }
                 } 
@@ -283,7 +278,7 @@ export default {
                     height: calc(200%);
                     margin-right: -20px;
                     display: none;
-                    @media only screen and (max-width: 1200px) {
+                    @media only screen and (max-width: 1400px) {
                         display: none !important;
                     }
                 }
