9da5ca4f58017f678e272c318611c5029cbe8670
WillYallop <hello@williamyallop.com>
2022-10-16 15:44:49 +0100

    Updated copy

--- site/loaders/homepage.ts
+++ site/loaders/homepage.ts
@@ -6,7 +6,7 @@ const homepageLoader: LoaderFunction = async () => {
     home: {
       title: "My projects",
       description:
-        "Hi, my name is Will and I'm a fullstack web developer. Below you'll find a selection of projects I've created over the years, ranging from websites, web apps, libraries and everything else in between. ",
+        "Hi, my name is Will and I'm a fullstack developer. Below you'll find a selection of projects I've created over the years, ranging from websites, web apps, libraries and everything else in between. ",
     },
   };
 };
